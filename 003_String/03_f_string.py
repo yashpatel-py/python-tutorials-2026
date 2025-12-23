e524fcf3ddf5339af250d057b89f16eb4a629961
Yash Patel <yashpatelpvt.21@gmail.com>
2025-12-23 17:38:43 -0500

    F string and In dexing

---
+++ 003_String/03_f_string.py
@@ -0,0 +1,12 @@
+first_name = "John"
+last_name = "Doe"
+full_name = f"My name is {first_name} {last_name}"
+print(full_name)
+
+
+price = 19.99
+quantity = 3
+total = f"Total: {price * quantity}"
+print(total)
+
+print(f"My name is {first_name.upper()} {last_name.upper()}")
---
+++ 003_String/04_string_indexing.py
@@ -0,0 +1,13 @@
+# String: Python
+# Index +ve: 0 1 2 3 4 5
+# Char:      P Y T H O N
+# Index -ve.-6-5-4-3-2-1
+
+
+word = "Python"
+
+# +ve indexing
+print(word[5])
+
+# -ve indexing
+print(word[-5])
