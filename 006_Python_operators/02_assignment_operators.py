b20d56bbc1e83241e0858fc2a962db22139be522
Yash Patel <yashpatelpvt.21@gmail.com>
2026-01-09 20:09:18 -0500

    Assignment Operators

---
+++ 006_Python_operators/02_assignment_operators.py
@@ -0,0 +1,70 @@
+# x = 10
+
+# x=y=z=100
+# print(x, y, z)
+
+# a,b,c=100,200,300
+# print(a,b,c)
+
+# --------------------
+# a = 10
+# a = a+5
+# print(a)
+
+# +=
+# a = 10
+# a += 5
+# print(a)
+
+# message = "Hello"
+# message+=" "
+# message+="World!"
+# print(message)
+
+# -=
+# balace = 1000
+# balace -= 250
+# print(balace)
+
+# *=
+# value = 5
+# value *= 3
+# print(value)
+
+# Example on *=
+# savings = 1000
+
+# savings *=2
+# print(f"Year 1: ${savings}")
+
+# savings *=2
+# print(f"Year 2: ${savings}")
+
+# savings *=2
+# print(f"Year 3: ${savings}")
+
+# /=
+# value = 100
+# value /= 4
+# print(value)
+
+# Example for /=
+# total_bill = 150
+# num_people = 3
+# total_bill /= num_people
+# print(f"Each person pays: ${total_bill}")
+
+# //=
+# value = 17
+# value //=5
+# print(value)
+
+# %=
+value = 17
+value %=5
+print(value)
+
+# **=
+value = 2
+value **= 3
+print(value)
