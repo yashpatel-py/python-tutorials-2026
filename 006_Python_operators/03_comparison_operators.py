c822fc89a89bf223e22a41a2f60218c5905b3b23
Yash Patel <yashpatelpvt.21@gmail.com>
2026-01-12 17:02:42 -0500

    Comparison Operators

---
+++ 006_Python_operators/03_comparison_operators.py
@@ -0,0 +1,43 @@
+# Operator    Name                    Example     Result
+# ==          Equal to                5 == 5      True
+# x = 10
+# y = 10
+
+# print(x==y)
+# print(5==10)
+# print(5==5.1)
+# print("hello"=="Hello")
+
+# !=          Not equal to            5 != 3      True
+# x = 10
+# y = 10
+
+# print(x!=y)
+# print(5!=10)
+# print(5!=5.1)
+# print("hello"!="Hello")
+
+
+# >           Greater than            5 > 3       True
+# print(5>3)
+# print(10>11)
+# print(10>5)
+# print(10>10)
+
+# <           Less than               5 < 3       False
+# print(5<3)
+# print(10<11)
+# print(10<5)
+# print(10<10)
+
+# >=          Greater than or equal   5 >= 5      True
+# print(5>=3)
+# print(10>=11)
+# print(10>=5)
+# print(10>=10)
+
+# <=          Less than or equal      5 <= 3      False
+print(5<=3)
+print(10<=11)
+print(10<=5)
+print(10<=10)
