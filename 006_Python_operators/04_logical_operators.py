f687bab256c062f1e960dd5309ce378efbd2fb10
Yash Patel <yashpatelpvt.21@gmail.com>
2026-01-12 17:37:44 -0500

    Logical Operators

---
+++ 006_Python_operators/04_logical_operators.py
@@ -0,0 +1,51 @@
+# AND - Both must be True
+# print(True and True)
+# print(True and False)
+
+# OR - At least one must be True
+# print(True or False)
+# print(False or False)
+
+# NOT - Reverses the value
+# print(not True)
+# print(not False)
+
+# ------- AND
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# age = 25
+# has_licanse = True
+
+# if age >= 18 and has_licanse:
+#     print("You can drive!")
+# else:
+#     print("You cannot drive.")
+
+# ------ OR
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# is_student = False
+# is_senior = True
+# is_member = False
+
+# if is_student or is_senior or is_member:
+#     print("You get a 10% discount!")
+# else:
+#     print("No discount available.")
+
+# ------- Not
+print(not True)
+print(not False)
+
+is_logged_in = False
+
+if not is_logged_in:
+    print("Please log in to continue")
+else:
+    print("Welcome back!")
