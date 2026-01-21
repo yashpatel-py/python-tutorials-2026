37522312e05e0f94224abd1ba7fbce74028460c2
Yash Patel <yashpatelpvt.21@gmail.com>
2026-01-20 23:26:44 -0500

    Identity Operators

---
+++ 006_Python_operators/05_identity_operators.py
@@ -0,0 +1,18 @@
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# x = a
+
+# print(a == b)
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+
+# print(id(x))
+# print(x is a)
+
+# ------ is not
+p = 1000
+q = 1000
+
+print(p is not q)
